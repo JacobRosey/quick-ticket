f275324c3305857fa99ec4a5ae49879029fb3e7f
JacobRosey <jrosey77@gmail.com>
2022-10-17 13:17:02 -0400

    Add express routes

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-console.log("started")
+const express = require('express');
+const mysql = require('mysql');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const bcrypt = require('bcryptjs');
+
+//Resume download not working
+
+const app = express();
+
+//Static files
+app.use(express.static('public'));
+
+//Templating Engine with handlebars
+app.engine('hbs', exphbs.engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+
+//Create db connection for heroku
+const db = mysql.createConnection(process.env.JAWSDB_URL);
+
+const port = process.env.PORT || 3000;
+ 
+const publicDirectory = path.join(__dirname, './public/');
+app.use(express.static(publicDirectory));
+
+//Parsing middleware
+app.use(express.urlencoded({ extended: false }));
+
+//Parse JSON
+app.use(express.json());
+
+//Connect to database
+db.connect((err) => {
+    if (err) throw err; 
+    console.log('Connected to DB');
+});
+
+app.listen(port, () => {
+    console.log(`Server started on Port ${port}`)
+});
+
+//Define Routes
+app.use('/', require('./routes/pages'));
+app.use('/login', require('./routes/pages'));
+//app.use('/auth', require('./routes/auth'));
+app.use('/register', require('./routes/pages'));
