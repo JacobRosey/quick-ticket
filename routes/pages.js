ca3afbff65ed4dfca56edc8d49375dd9b71bb3f3
JacobRosey <jrosey77@gmail.com>
2022-10-20 19:30:33 -0400

    register page

--- routes/pages.js
+++ routes/pages.js
@@ -8,6 +8,10 @@ router.get('/', (req, res) => {
     res.render('login');
 });
 
+router.get('/register', (req, res) => {
+    res.render('register');
+});
+
 router.get('/home', (req, res) => {
     res.render('index');
 });
