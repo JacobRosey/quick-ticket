61d62ede930c46a390d4a2be33b7b605ecc42e39
JacobRosey <jrosey77@gmail.com>
2022-10-23 16:16:17 -0400

    login logic

--- public/index.js
+++ public/index.js
@@ -1,3 +1,24 @@
 function joinTeam(){
     alert("Hello")
 }
+
+function getLogin() {
+    let isLogged = sessionStorage.getItem('logged');
+    console.log(isLogged)
+    if (isLogged === 'true') {
+        return alert("You're already logged in!")
+    } else {
+        let user = document.getElementById('loginUser').value;
+        let pass = document.getElementById('loginPass').value;
+
+        console.log("Getting login data...")
+
+        let data = {
+            "user": user,
+            "pass": pass
+        }
+        console.log('now do ajax function with ' + data)
+        //ajaxFunc(undefined, '/login', "GET", data);
+    }
+
+}
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,5 @@ const authController = require('../controllers/auth');
 const router = express.Router();
 
 router.post('/register', authController.register);
-//router.post('/database', authController.editRow);
 
 module.exports = router;
