4fdf8c675d8a604a555f07a62ad3eddadb19b619
JacobRosey <jrosey77@gmail.com>
2022-10-24 21:55:49 -0400

    logo href

--- routes/pages.js
+++ routes/pages.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('login');
+    res.render('home');
 });
 
 router.get('/register', (req, res) => {
