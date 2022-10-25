06e3c26fbbbfddfae9b6053881f38071bc4699b5
JacobRosey <jrosey77@gmail.com>
2022-10-24 21:58:43 -0400

    fix

--- routes/pages.js
+++ routes/pages.js
@@ -12,10 +12,6 @@ router.get('/register', (req, res) => {
     res.render('register');
 });
 
-router.get('/home', (req, res) => {
-    res.render('index');
-});
-
 router.get('/register', (req, res) => {
     res.render('register');
 });
