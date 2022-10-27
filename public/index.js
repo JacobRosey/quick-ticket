a2d0a241543125ec09b32fd357f25d4cae5bb6ad
JacobRosey <jrosey77@gmail.com>
2022-10-26 21:09:55 -0400

    underline

--- public/index.js
+++ public/index.js
@@ -21,6 +21,7 @@ function setActiveLink(){
         let text = document.getElementById(active);
         text.style.fontWeight = 'bolder';
         if(active.includes('ticket')){
+            document.getElementById('tickets').style.fontStyle = "underline"
             let navLink = document.getElementById('submenu1');
             navLink.className ="nav flex-column ms-1 collapse show";
             navLink.setAttribute("aria-expanded", "true");
