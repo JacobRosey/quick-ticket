ed6aa632009adb7b5a363748540b10f88b6e05fe
JacobRosey <jrosey77@gmail.com>
2022-10-26 21:03:32 -0400

    change contains to includes for strings

--- public/index.js
+++ public/index.js
@@ -20,7 +20,7 @@ function setActiveLink(){
     if(active !== 'login' && active !== 'register'){
         let text = document.getElementById(active);
         text.style.fontWeight = 'bolder';
-        if(active.contains('ticket')){
+        if(active.includes('ticket')){
             let navLink = document.getElementById('submenu1');
             navLink.className("nav flex-column ms-1 collapse show");
             navLink.setAttribute("aria-expanded", true);
