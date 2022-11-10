e2e4286b87101f0a9955f767d49e0ec411c245c3
JacobRosey <jrosey77@gmail.com>
2022-11-10 12:37:21 -0500

    ffs

--- app.js
+++ app.js
@@ -294,7 +294,7 @@ app.route('/team/:user')
                                 console.log(err)
                             }
                             if(result.length == 0){
-                                dbPromise.reject(new Error("error occurred in finding team name"));
+                                Promise.reject(new Error("error occurred in finding team name"));
                             }else{
                                 names += result[i].team_name;
                             }
