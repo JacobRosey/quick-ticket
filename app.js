18f920d2953a575da4285866ae57a772df0e24ea
JacobRosey <jrosey77@gmail.com>
2022-11-10 14:38:17 -0500

    testing

--- app.js
+++ app.js
@@ -292,7 +292,7 @@ app.route('/team/:user')
                             if (err) {
                                 console.log(err)
                             }
-                            names += result[i].team_name;
+                            names[i] = result[i].team_name;
                         })
                     }
                     console.log('names are' + names);
