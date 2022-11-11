c143e201fec74713b5e5858f6392f58ff0dd9897
JacobRosey <jrosey77@gmail.com>
2022-11-11 00:11:18 -0500

    promise

--- app.js
+++ app.js
@@ -305,8 +305,7 @@ app.route('/team/:user')
                 }
 
             })
-        })
-        dbPromise.then(() => {
+        }).then(() => {
             var myTeams = [];
             for (let i = 0; i < teamIDs.length; i++) {
                 db.query("SELECT * FROM Teams WHERE team_id = " + teamIDs[i].team_id + "", (err, result) =>{
