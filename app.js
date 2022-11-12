0aff6697a912e553383e8e490fbc19486b6a4da7
JacobRosey <jrosey77@gmail.com>
2022-11-11 21:26:10 -0500

    comment

--- app.js
+++ app.js
@@ -295,7 +295,7 @@ app.route('/team/:user')
                             for (let i = 0; i < teamIDs.length; i++) {
                                 console.log('sending team id ' + teamIDs[i] + ' to asyncQuery')
                                 let result = await asyncQuery(teamIDs[i]);
-                                await new Promise(resolve => setTimeout(resolve, 1000));
+                                //await new Promise(resolve => setTimeout(resolve, 1000));
                                 array.push(result)
                                 console.log('just pushed ' + result + ' to array')
                             }
