8fb540773d3d545e957c6042a0cf45a9e5cf298c
JacobRosey <jrosey77@gmail.com>
2022-11-15 11:49:31 -0500

    stringify

--- app.js
+++ app.js
@@ -306,14 +306,15 @@ app.route('/team/:user')
                             /*if (err) {
                                 console.log(err)
                             }*/
+                            //Can't use normal db.query syntax here because it uses a callback
                             const result = await db.query("SELECT * FROM Teams WHERE team_id = " + id + "");
                             console.log('pushing result to array for id ' + id + '. The result is ' + result);
                             return result;
                             //})
                         }
                         loopIndices().then(response => {
-                            console.log('response is: ' + response);
-                            res.send(response);
+                            console.log('response is: ' + JSON.stringify(response));
+                            res.send(JSON.stringify(response));
                         }).catch(err => {
                             console.log(err);
                         });
