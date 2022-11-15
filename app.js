58b3adf80a90e59d74e3e4c5d7bef80b169575ac
JacobRosey <jrosey77@gmail.com>
2022-11-15 14:43:02 -0500

    arr

--- app.js
+++ app.js
@@ -305,13 +305,15 @@ app.route('/team/:user')
                             /*if (err) {
                                 console.log(err)
                             }*/
+                            let arr = [];
                             //Can't use normal db.query syntax here because it uses a callback
                             const result = await db.query("SELECT * FROM Teams WHERE team_id = " + id + "");
-                            if(result.length > 0){
+                            arr.push(result);
+                            if(arr.length > 0){
                                 console.log('there are results');
                             }
-                            console.log('pushing result to array for id ' + id + '. The result is ' + result);
-                            return result;
+                            console.log('pushing result to array for id ' + id + '. The result is ' + arr[arr.length]);
+                            return arr;
                             //})
                         }
 
