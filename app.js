2e70a1150d24d8f9764352fe865ac2c2142f28ad
JacobRosey <jrosey77@gmail.com>
2022-11-16 14:12:22 -0500

    mysql2/promise

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const exphbs = require('express-handlebars');
 const path = require('path');
 const bcrypt = require('bcryptjs');
