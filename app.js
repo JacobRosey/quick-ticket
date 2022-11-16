1007d0add98db1380c3481bb9509303bef899e6f
JacobRosey <jrosey77@gmail.com>
2022-11-15 23:09:45 -0500

    test

--- app.js
+++ app.js
@@ -313,7 +313,7 @@ app.route('/team/:user')
                         //Gonna come back to this
                         loopIndices().then(response => {
                             //res.json(response)
-                            console.log(JSON.parse(response));
+                            console.log(response.team_name);
                             res.send('bruh');
                     }).catch(err => {
                         console.log('You caught this error: ' + err);
