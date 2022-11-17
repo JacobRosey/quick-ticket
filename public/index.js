e11a17d35f4b99e4bf4af5eda2ce17456f531c85
JacobRosey <jrosey77@gmail.com>
2022-11-17 18:39:48 -0500

    err

--- public/index.js
+++ public/index.js
@@ -171,7 +171,7 @@ function useResponse(res) {
 
 function deleteTeam(num) {
     const teamInfoCards = document.getElementsByClassName('team-info');
-    const teamName = document.getElementsByClassName('team-name-span')[num];
+    const teamName = document.getElementsByClassName('team-name-span');
     console.log(teamName[num].innerHTML);
     if (confirm('Are you sure you want to delete ' + teamName.innerHTML + '? This cannot be reversed.')) {
         //Need to fix the alert box, there  are line breaks in the html apparently
