085fea2a563d84777cde069867bf91da4ba6d7a3
JacobRosey <jrosey77@gmail.com>
2022-11-17 13:42:38 -0500

    not on team

--- public/index.js
+++ public/index.js
@@ -131,7 +131,18 @@ function useResponse(res) {
     const container = document.getElementById('team-card-container');
     
     if (res == 0) {
-        container.innerHTML += `<h3 class="text-center" style="font-weight: bold;">You are not on a team!</h3>`
+        container.innerHTML += `<i class="fs-4 bi-people"></i><h3 class="text-center" style="font-weight: bold;">You are not on a team!</h3>`
+        container.innerHTML += `
+        <div class="jumbotron">
+        <h1 class="display-4">Hello, world!</h1>
+        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr class="my-4">
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <p class="lead">
+          <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </p>
+      </div>
+        `
     }
 
     if (res[0].hasOwnProperty('team_id') && res[0].hasOwnProperty('team_name')) {
