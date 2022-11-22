ec6e30c995835e50985097c86a6a4b124b16290b
JacobRosey <jrosey77@gmail.com>
2022-11-22 12:28:09 -0500

    revert

--- app.js
+++ app.js
@@ -520,7 +520,7 @@ app.route('/newticket/:user/:team/:title/:prio/:desc')
                 if (err) {
                     console.log(err)
                 }
-                resolve(result.team_id)
+                resolve(result)
             })
         })
         dbPromise.then((id) => {
