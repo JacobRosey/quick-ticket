03df31efc25c9bd625c4cd2cb55c16ed5756d66d
JacobRosey <jrosey77@gmail.com>
2022-11-28 14:39:42 -0500

    ticket id

--- app.js
+++ app.js
@@ -672,13 +672,11 @@ app.route('/mytickets/:user')
                             db.promise().query("SELECT * FROM Tickets WHERE team_id = " + response[i])
                                 .then(([rows, fields]) => {
                                     arr.push(rows[0])
+                                    db.promise().query("SELECT * FROM Ticket_Data WHERE ticket_id = " + arr[i].ticket_id)
+                                        .then(([rows, fields]) => {
+                                            arr.push(rows[0])
+                                        }).catch(err => console.log(err))
                                 }).catch(err => console.log(err))
-                        }
-                        for(let i=0; i<response.length; i++){
-                            db.promise().query("SELECT * FROM Ticket_Data WHERE team_id =" + response[i])
-                                .then(([rows, fields]) => {
-                                    arr.push(rows[0])
-                                }).catch(err => {console.log(err)})
                         }
                         resolve(arr);
                     }, 50)
