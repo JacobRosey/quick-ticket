2c8a1ec44b01bdc48be248b56ddeae257af8bdc2
JacobRosey <jrosey77@gmail.com>
2022-11-28 15:14:16 -0500

    continue in for loop

--- public/index.js
+++ public/index.js
@@ -352,6 +352,10 @@ function useResponse(res) {
                 for(let i=0; i<res.length; i++){
                     let id = res[i].ticket_id;
                     for(let j=0; j<res.length; j++){
+                        //Need to avoid comparing an index to itself
+                        if(i == j){
+                            continue;
+                        }
                         if(res[j].ticket_id == id){
                             console.log("ticket ids match: " + res[j].ticket_id)
                             res[i].ticket_desc = res[j].ticket_desc;
