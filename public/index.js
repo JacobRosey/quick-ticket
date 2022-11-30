007625918b401a471cbbff54487e071d1df62e00
JacobRosey <jrosey77@gmail.com>
2022-11-29 22:25:41 -0500

    test

--- public/index.js
+++ public/index.js
@@ -364,10 +364,13 @@ function useResponse(res) {
                 for (let j = 0; j < res.length; j++) {
                     //Need to avoid comparing an index to itself
                     console.log(res[i].ticket_id, res[j].ticket_id)
-                    if (i == j) {
-                        continue;
+                    if(res.length > 2 ){
+                        if (i == j) {
+                            continue;
+                        }
                     }
-                    if (res[j] == null || res[j] =='') {
+                    
+                    if (res[j] == null || res[j] == '') {
                         res.splice(j, 1)
                         console.log('spliced!');
                         console.log('new arr: '+ JSON.stringify(res))
