c35edfd4898d9b4049ac8bf9eac88dc7164562a2
JacobRosey <jrosey77@gmail.com>
2022-12-12 15:37:09 -0500

    json.parse

--- app.js
+++ app.js
@@ -772,6 +772,7 @@ app.route('/ticketdata/:user/:status')
                                             db.promise().query("SELECT * FROM Ticket_Data WHERE ticket_id = " + arr[i].ticket_id)
                                                 .then(([rows, fields]) => {
                                                     console.log('Inside the second query')
+                                                    console.log(JSON.parse(rows))
                                                     console.log('Pushing this row: ' + rows)
                                                     arr.push(rows)
                                             }).catch(err => console.log(err))
