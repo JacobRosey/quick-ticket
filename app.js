c2c92169a17ede312baae7719137022a31fdb7c7
JacobRosey <jrosey77@gmail.com>
2022-12-12 15:14:38 -0500

    index

--- app.js
+++ app.js
@@ -767,7 +767,7 @@ app.route('/ticketdata/:user/:status')
                                         arr.push(rows)
                                         return i;
                                     }).then((i) => {
-                                        console.log('Should be doing second .map now')
+                                        console.log('Should be doing second .map now for index '+i)
                                             db.promise().query("SELECT * FROM Ticket_Data WHERE ticket_id = " + arr[i].ticket_id)
                                                 .then(([rows, fields]) => {
                                                     console.log('Pushing this row: ' + rows[0])
