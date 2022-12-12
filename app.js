e6ab6d50060ae3b58188070da5573955349e9ef0
JacobRosey <jrosey77@gmail.com>
2022-12-12 14:30:12 -0500

    log

--- app.js
+++ app.js
@@ -748,7 +748,8 @@ app.route('/ticketdata/:user/:status')
                                 console.log('Querying for this index: ' + index);
                                 db.promise().query("SELECT * FROM Ticket_Data WHERE ticket_id = " + arr[index].ticket_id)
                                     .then(([rows, fields]) => {
-                                        console.log('Pushing this row: ' + rows[0])
+                                        console.log('Pushing this row: ' + rows[0]);
+                                        console.log('Inside the second Promise.all')
                                         arr.push(rows[0])
                                     }).catch(err => console.log(err))
                             }))
@@ -772,6 +773,7 @@ app.route('/ticketdata/:user/:status')
                                 db.promise().query("SELECT * FROM Ticket_Data WHERE ticket_id = " + arr[index].ticket_id)
                                     .then(([rows, fields]) => {
                                         console.log('Pushing this row: ' + rows[0])
+                                        console.log('Inside the second Promise.all')
                                         arr.push(rows[0])
                                     }).catch(err => console.log(err))
                             }))
