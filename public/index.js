20ba4318877ec528267c9e9eb4d04fd1c7e8d007
JacobRosey <jrosey77@gmail.com>
2022-12-26 12:11:15 -0500

    button width

--- public/index.js
+++ public/index.js
@@ -371,8 +371,8 @@ function useResponse(res) {
                     <li class="list-group-item" id="member-count">Total Members: <span style="font-weight: 300;">` + res[i].member_count + `</span></li>
                     <li class="list-group-item">Team Admin: <span id="admin-name" style="font-weight: 300;">`+ res[i].admin_name + `</span></li>
                 </ul>
-                <a class="btn btn-warning" onClick="leaveTeam(`+ i + `)" role="button" style="font-weight: bold; line-height: 32.5px !important;">Leave Team</a>
-                <a class="btn btn-danger" onClick="deleteTeam(`+ i + `)" role="button" style="font-weight: bold; line-height: 32.5px !important;">Delete Team</a>
+                <a class="btn btn-warning" onClick="leaveTeam(`+ i + `)" role="button" style="font-weight: bold; width: 50%; line-height: 32.5px !important;">Leave Team</a>
+                <a class="btn btn-danger" onClick="deleteTeam(`+ i + `)" role="button" style="font-weight: bold; width: 50%; line-height: 32.5px !important;">Delete Team</a>
                 </div>
                 </div>
                 </div>
