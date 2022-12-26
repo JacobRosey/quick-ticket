db4a2c888853d04b7ab9f33f23a8608b41f1d70c
JacobRosey <jrosey77@gmail.com>
2022-12-26 12:36:48 -0500

    warning

--- public/index.js
+++ public/index.js
@@ -372,8 +372,8 @@ function useResponse(res) {
                     <li class="list-group-item">Team Admin: <span id="admin-name" style="font-weight: 300;">`+ res[i].admin_name + `</span></li>
                 </ul>
                 <div class="btn-group" role="group" style="display: flex;">
-                <a class="btn btn-danger" onClick="leaveTeam(`+ i + `)" role="button" style="font-weight: bold; width: 50%; line-height: 32.5px !important;">Leave Team</a>
-                <a class="btn btn-outline-danger" onClick="deleteTeam(`+ i + `)" role="button" style="font-weight: bold; width: 50%; line-height: 32.5px !important;">Delete Team</a>
+                <a class="btn btn-warning" onClick="leaveTeam(`+ i + `)" role="button" style="font-weight: bold; width: 50%; line-height: 32.5px !important;">Leave Team</a>
+                <a class="btn btn-danger" onClick="deleteTeam(`+ i + `)" role="button" style="font-weight: bold; width: 50%; line-height: 32.5px !important;">Delete Team</a>
                 </div>
                 </div>
                 </div>
