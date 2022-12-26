627358fdbd677a6f7356353c468e7f2349f4f40f
JacobRosey <jrosey77@gmail.com>
2022-12-26 12:14:58 -0500

    display flex

--- public/index.js
+++ public/index.js
@@ -371,7 +371,7 @@ function useResponse(res) {
                     <li class="list-group-item" id="member-count">Total Members: <span style="font-weight: 300;">` + res[i].member_count + `</span></li>
                     <li class="list-group-item">Team Admin: <span id="admin-name" style="font-weight: 300;">`+ res[i].admin_name + `</span></li>
                 </ul>
-                <div style="display: inline-block;">
+                <div style="display: flex;">
                 <a class="btn btn-warning" onClick="leaveTeam(`+ i + `)" role="button" style="font-weight: bold; width: 50%; line-height: 32.5px !important;">Leave Team</a>
                 <a class="btn btn-danger" onClick="deleteTeam(`+ i + `)" role="button" style="font-weight: bold; width: 50%; line-height: 32.5px !important;">Delete Team</a>
                 </div>
