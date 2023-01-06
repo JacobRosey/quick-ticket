2c5871bcd722a8db7fe168ec4b85027e23584b46
JacobRosey <jrosey77@gmail.com>
2023-01-06 13:39:43 -0500

    test

--- public/index.js
+++ public/index.js
@@ -414,6 +414,7 @@ function useResponse(res) {
         }
         //Before new ticket creation
         if (res[0] == 'team_names') {
+            console.log('length is: ' + res.length)
             if(res.length == 1){
                 const container = document.getElementsByClassName('container')
                 document.getElementsByClassName('form-group').style.display = "none";
