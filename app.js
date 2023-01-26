841a9b276feba921109204af1c8401c97e89e457
JacobRosey <jrosey77@gmail.com>
2023-01-26 17:22:24 -0500

    res.send

--- app.js
+++ app.js
@@ -876,8 +876,7 @@ app.route('/leave-team')
                                     }
 
                                 })
-
-                                res.send('New admin: ' + teamMembers, teamID.toString())
+                                res.status(200).send('New admin: ' + teamMembers, teamID.toString())
                             }
                         })
                     })
