af5fe7535bd3a8917b2517e97cbf434f40aad95a
JacobRosey <jrosey77@gmail.com>
2023-01-27 14:00:52 -0500

    test

--- public/index.js
+++ public/index.js
@@ -456,25 +456,51 @@ function useResponse(res) {
             }
             const teamID = arr[arr.length - 1];
             const container = document.getElementById('team-container');
-            console.log('teamID is - '+ teamID);
+            console.log('teamID is - ' + teamID);
             container.innerHTML +=
                 `
-            <div id="myModal" class="modal">
-            <div class="modal-content">
-                <span class="close">&times;</span>
-                <form>
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" required>
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required>
-                <input type="checkbox" id="rememberMe" name="rememberMe">
-                <label for="rememberMe">Remember me</label>
-                <button type="submit">Log in</button>
-                </form>
-            </div>
-            </div>
-
+                <button id="myBtn">Open Modal</button>
+                <div id="myModal" class="modal">
+                <div class="modal-content">
+                    <span class="close">&times;</span>
+                    <form>
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" required>
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password" required>
+                    <input type="checkbox" id="rememberMe" name="rememberMe">
+                    <label for="rememberMe">Remember me</label>
+                    <button type="submit">Log in</button>
+                    </form>
+                </div>
+                </div>
             `
+            // Get the button that opens the modal
+            var btn = document.getElementById("myBtn");
+
+            // Get the modal
+            var modal = document.getElementById("myModal");
+
+            // Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close")[0];
+
+            // When the user clicks the button, open the modal 
+            btn.onclick = function () {
+                modal.style.display = "block";
+            }
+
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function () {
+                modal.style.display = "none";
+            }
+
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function (event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+
 
             return;
         }
