7cf687c23b2d4ee1fbb3e4c1c81c888074de6679
JacobRosey <jrosey77@gmail.com>
2023-01-27 14:42:46 -0500

    css

--- public/index.js
+++ public/index.js
@@ -461,7 +461,7 @@ function useResponse(res) {
             container.innerHTML +=
                 `
                 <button id="myBtn" style="visibility:hidden;">Open Modal</button>
-                <div id="myModal" class="modal" style="width: 80%; position:absolute;">
+                <div id="myModal" class="modal">
                 <div class="modal-content">
                     <span class="close">&times;</span>
                     <h5 class="modal-title">Leave Team</h5>
