d099ca2b74648ba4597d88b4bb4cd695e1807ebc
JacobRosey <jrosey77@gmail.com>
2023-01-27 21:37:48 -0500

    modal

--- public/index.js
+++ public/index.js
@@ -471,7 +471,7 @@ function useResponse(res) {
                 <div id="myModal" class="modal">
                 <div class="modal-content">
                 <div class="modal-header">
-                <h5 class="modal-title">Modal title</h5>
+                <h5 class="modal-title">Leave Team</h5>
                     <button type="button" id="close" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <p>Please transfer admin privileges to a team member before leaving!</p>
