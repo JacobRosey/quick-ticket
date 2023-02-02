696a7736b9693eaac6fd4e48972eac16ef18c8a1
JacobRosey <jrosey77@gmail.com>
2023-02-02 16:00:57 -0500

    colors

--- public/index.js
+++ public/index.js
@@ -798,9 +798,9 @@ function useResponse(res) {
         closedHTML.innerHTML += res[0].tickets_closed;
         holdHTML.innerHTML += res[1];
 
-        openHTML.style = "--size: calc(" + res[0].tickets_opened + " / " + topOfRange + ");"
-        closedHTML.style = "--size: calc(" + res[0].tickets_closed + " / " + topOfRange + ");"
-        holdHTML.style = "--size: calc(" + res[1] + " / " + topOfRange + ");"
+        openHTML.style = "--size: calc(" + res[0].tickets_opened + " / " + topOfRange + "); --color: #198753;"
+        closedHTML.style = "--size: calc(" + res[0].tickets_closed + " / " + topOfRange + "); --color: #dd3444;"
+        holdHTML.style = "--size: calc(" + res[1] + " / " + topOfRange + "); --color: #0e6efd;"
 
     }
 
