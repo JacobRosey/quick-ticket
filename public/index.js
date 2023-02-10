af903dde37332ef59135a43a50ff0480ca6d6fbf
JacobRosey <jrosey77@gmail.com>
2023-02-10 14:09:38 -0500

    col

--- public/index.js
+++ public/index.js
@@ -934,14 +934,14 @@ function useResponse(res) {
                 
                     <caption>Past Week Statistics</caption>
                     <tr>
-                    <th scope="row">
+                    <th scope="col">
                     <i class="bi bi-circle" style="transform: scale(1.5)" data-toggle="tooltip"
                         data-placement="top" title="Tickets Opened"></i>
                     </th>
                         <td style="--color: #ff914c;--size: calc(` + pastWeekOpened.length + ` / ` + topOfRange + `);">` + pastWeekOpened.length + `&nbsp;&nbsp;</td>
                     </tr>
                     <tr>
-                    <th scope="row"> <i class="bi bi-x-circle" style="transform: scale(1.5)"
+                    <th scope="col"> <i class="bi bi-x-circle" style="transform: scale(1.5)"
                     data-toggle="tooltip" data-placement="top" title="Tickets Closed"></i>
                     </th>
                         <td style="--color: #c084f5;--size: calc(` + pastWeekClosed.length + ` / ` + topOfRange + `);">` + pastWeekClosed.length + `&nbsp;&nbsp;</td>
