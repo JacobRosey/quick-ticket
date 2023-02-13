5874a51a3751d3fd8238c2e6f7c98c1052d5cb73
JacobRosey <jrosey77@gmail.com>
2023-02-13 16:31:48 -0500

    cs

--- public/index.js
+++ public/index.js
@@ -937,13 +937,13 @@ function useResponse(res) {
                     <i class="bi bi-circle" style="transform: scale(1.5)" data-toggle="tooltip"
                         data-placement="top" title="Tickets Opened"></i>
                     </th>
-                        <td style="--color: #ff914c;--size: calc(` + pastWeekOpened.length + ` / ` + topOfRange + `); margin: .75em 1.5em .75em 1.5em; ">` + pastWeekOpened.length + `&nbsp;&nbsp;</td>
+                        <td style="--color: #ff914c;--size: calc(` + pastWeekOpened.length + ` / ` + topOfRange + `); margin: auto; ">` + pastWeekOpened.length + `&nbsp;&nbsp;</td>
                     </tr>
                     <tr>
                     <th scope="col"> <i class="bi bi-x-circle" style="transform: scale(1.5)"
                     data-toggle="tooltip" data-placement="top" title="Tickets Closed"></i>
                     </th>
-                        <td style="--color: #c084f5;--size: calc(` + pastWeekClosed.length + ` / ` + topOfRange + `); margin: .75em 1.5em .75em 1.5em;">` + pastWeekClosed.length + `&nbsp;&nbsp;</td>
+                        <td style="--color: #c084f5;--size: calc(` + pastWeekClosed.length + ` / ` + topOfRange + `); margin: auto;">` + pastWeekClosed.length + `&nbsp;&nbsp;</td>
                     </tr>
                     </tbody> 
                 </table>
