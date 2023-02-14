947ec020b884ae3c55af48d0abed10104d1d5960
JacobRosey <jrosey77@gmail.com>
2023-02-14 14:48:53 -0500

    css

--- public/index.js
+++ public/index.js
@@ -956,13 +956,13 @@ function useResponse(res) {
                     <i class="bi bi-circle" style="transform: scale(1.5)" data-toggle="tooltip"
                         data-placement="top" title="Tickets Opened"></i>
                     </th>
-                        <td style="--color: #ff914c;--size: calc(` + pastWeekOpened.length + ` / ` + topOfRange + `); margin-left: 1em; margin-right: 1em; margin-bottom: .5em;">` + pastWeekOpened.length + `&nbsp;&nbsp;</td>
+                        <td style="--color: #f56c68;--size: calc(` + pastWeekOpened.length + ` / ` + topOfRange + `); margin-left: 1em; margin-right: 1em; margin-bottom: .5em;">` + pastWeekOpened.length + `&nbsp;&nbsp;</td>
                     </tr>
                     <tr>
                     <th scope="col"> <i class="bi bi-x-circle" style="transform: scale(1.5)"
                     data-toggle="tooltip" data-placement="top" title="Tickets Closed"></i>
                     </th>
-                        <td style="--color: #c084f5;--size: calc(` + pastWeekClosed.length + ` / ` + topOfRange + `); margin-left: 1em; margin-right: 1em; margin-bottom: .5em;">` + pastWeekClosed.length + `&nbsp;&nbsp;</td>
+                        <td style="--color: #87dd7f;--size: calc(` + pastWeekClosed.length + ` / ` + topOfRange + `); margin-left: 1em; margin-right: 1em; margin-bottom: .5em;">` + pastWeekClosed.length + `&nbsp;&nbsp;</td>
                     </tr>
                     </tbody> 
                 </table>
