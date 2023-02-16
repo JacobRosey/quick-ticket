2136e0dd9c1178d3c76aae461af1ac99eb1b8487
JacobRosey <jrosey77@gmail.com>
2023-02-16 13:55:22 -0500

    css

--- public/index.js
+++ public/index.js
@@ -861,7 +861,7 @@ function useResponse(res) {
             const container = document.getElementById('chart-container');
             container.innerHTML +=
                 `
-                    <table id="performance-chart" class="charts-css bar show-heading show-labels">
+                    <table id="performance-chart" class="charts-css bar show-heading show-labels" style="margin-bottom: 1em;">
                         <caption>All-Time Statistics</caption>
                         <tbody>
                             <tr>
@@ -975,7 +975,7 @@ function useResponse(res) {
 
             container.innerHTML +=
                 `
-                <table class="charts-css bar show-heading show-labels" id="past-week-chart">
+                <table class="charts-css bar show-heading show-labels" id="past-week-chart" style="margin-bottom: 1em;">
                 <caption>Past Week Statistics</caption>    
                 <tbody>
                     <tr>
