0cdb6febdd9c121077a645fe3883ed1c6e1a8ce2
JacobRosey <jrosey77@gmail.com>
2023-02-17 13:57:15 -0500

    registration alert

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,48 +4,46 @@ const bcrypt = require('bcryptjs');
 //Connect to Jaws DB 
 const db = mysql.createConnection(process.env.JAWSDB_URL);
 
-exports.register = (req, res) => {
+exports.register = async (req, res) => {
     console.log(req.body);
 
     const { user, password, passwordConfirm } = req.body;
 
-    db.query('SELECT user_name FROM users WHERE user_name = ?', [user], async (err, result) => {
-        if (err) {
-            console.log(err);
-        }
+    try {
+        const existingUser = await db.query('SELECT user_name FROM users WHERE user_name = ?', [user]);
+
         if (user.length <= 6) { 
-            //Need to make these messages show a red banner,
-            //right now it shows green banner as if the registration succeeded
             return res.render('register', {
                 failed: 'Username must be 6 or more characters'
             });
         }
-        if (result.length > 0) {
+
+        if (existingUser.length > 0) {
             return res.render('register', {
                 failed: 'That username is not available!'
             })
-        } else if (password !== passwordConfirm) {
+        }
+
+        if (password !== passwordConfirm) {
             return res.render('register', {
                 failed: 'Your passwords do not match!'
             });
         }
-        //Hash the password 8 times using bcrypt
+
         let hashedPass = await bcrypt.hash(password, 8);
         console.log(hashedPass);
-        db.query('INSERT INTO Users SET ?', { user_name: user, user_hash: hashedPass }, (err, result) => {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                res.render('register', {
-                    success: 'User Registered!'
-                });
-                res.redirect('/login')
-            }
-        }
-        )
-    });
-}
+        await db.query('INSERT INTO Users SET ?', { user_name: user, user_hash: hashedPass });
+
+        // Redirect to the login page with a success message
+        res.redirect('/login?success=Registration%20successful.%20Please%20log%20in.');
+
+    } catch (err) {
+        console.log(err);
+        res.render('register', {
+            failed: 'An error occurred while processing your request. Please try again later.'
+        });
+    }
+};
 
 exports.newTicket = (req, res) => {
 
