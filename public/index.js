623d117138c66c7eb94412db0291f85bf9bc63fc
JacobRosey <jrosey77@gmail.com>
2023-02-21 22:04:52 -0500

    css

--- public/index.js
+++ public/index.js
@@ -633,7 +633,7 @@ function useResponse(res) {
             console.log(res);
             teamSelectContainer.innerHTML +=
                 `<div class="form-group" style="animation: slide-in .75s ease-in-out;
-                transform-origin: right;">
+                transform-origin: left;">
                 <label for="exampleFormControlSelect1">Select Team</label>
                 <select class="form-control" name="team-select-input" id="team-select-input" required>
                 </select>
