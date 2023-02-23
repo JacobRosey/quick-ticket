196c29f6fc32419ab6198f36538664a1cb7cdfcd
JacobRosey <jrosey77@gmail.com>
2023-02-23 12:55:59 -0500

    remove html

--- public/index.js
+++ public/index.js
@@ -954,6 +954,10 @@ function useResponse(res) {
 
             //Create recent activity chart for actions completed in the past week
             const container = document.getElementById('chart-container');
+            const placeholders = document.getElementsByClassName('placeholder');
+            for(let i=0; i<placeholders.length; i++){
+                placeholders[i].remove();
+            }
 
             container.innerHTML +=
                 `
