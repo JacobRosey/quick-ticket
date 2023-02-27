633e076d15c88e485eba9b12b4db23794c6c4ce6
JacobRosey <jrosey77@gmail.com>
2023-02-27 16:59:37 -0500

    syntax

--- public/index.js
+++ public/index.js
@@ -328,7 +328,7 @@ function deleteTeam(num) {
 
 function inviteNewMember(index){
     const teamName = document.querySelectorAll('.team-name-span')[index].innerHTML.trim();
-    const listGroup = document.querySelectorAll('.list-group .list-group-flush .index-'+i);
+    const listGroup = document.querySelectorAll('.list-group .list-group-flush .index-'+index);
     listGroup.style.backgroundColor="green";
 
     alert('You clicked the element for ' + teamName);
