7d90a8ca179398b3e6ac490824bba22bce681c37
JacobRosey <jrosey77@gmail.com>
2023-02-28 12:58:33 -0500

    ajax

--- public/index.js
+++ public/index.js
@@ -393,14 +393,14 @@ function sendTeamInvite(teamName){
     var modal = document.getElementById("myModal");
     
     modal.remove();
-    /*
+    
     const newMember = document.getElementById('username').value;
     const data = {
         user: newMember,
         team: teamName
     }
 
-    ajaxFunc('/invite-member/:user/:team', "POST", data);*/
+    ajaxFunc('/invite-member/:user/:team', "POST", data);
 }
 function ajaxFunc(path, method, d) {
 
