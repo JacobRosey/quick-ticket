084b9e7ebcc2dd1274c4806568b40eb30279d097
JacobRosey <jrosey77@gmail.com>
2023-02-28 12:45:41 -0500

    syntax

--- public/index.js
+++ public/index.js
@@ -350,7 +350,7 @@ function inviteNewMember(index) {
                 </form>
                 </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-primary" onClick="inviteNewMember(`+ noHyphens +`);">Confirm</button>
+                <button type="button" class="btn btn-primary" onClick="sendTeamInvite(`+ noHyphens +`);">Confirm</button>
                 </div>
             </div>
             </div>
