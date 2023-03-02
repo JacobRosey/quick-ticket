db6f1da0e85bbc34644bc1e22659eac4b12e1586
JacobRosey <jrosey77@gmail.com>
2023-03-02 14:04:44 -0500

    css

--- public/index.js
+++ public/index.js
@@ -573,9 +573,10 @@ function useResponse(res) {
             for(let i=0; i<res.length; i++){
                 inviteContainer.innerHTML +=
                     `
-                    <p>`+res[i].team_name +`</p>
+                    <div display="flex"><span>`+res[i].team_name +`</span>
                     <a class="btn btn-secondary" onclick="teamInviteResponse(true, `+res[i]+`)" role="button" style="font-weight: bold; width: 50%; line-height: 32.5px !important;">Accept</a>
                     <a class="btn btn-danger" onclick="teamInviteResponse(false, `+res[i]+`)" role="button" style="font-weight: bold; width: 50%; line-height: 32.5px !important;">Decline</a>
+                    </div>
                     `
             }
         } else {
