504d568b9b7f9bd89a8b4bed76bda0647c88d44c
JacobRosey <jrosey77@gmail.com>
2023-03-02 14:19:30 -0500

    role

--- public/index.js
+++ public/index.js
@@ -563,7 +563,7 @@ function useResponse(res) {
                     <div class="card-body">
                         <h5 class="card-title">`+ title + `</h5>
                         <p class="card-text">Accept or decline the following invitations</p>
-                        <div class="btn-group" id="team-invite-btn" role="group" style="background-color: #f7f7f7;">
+                        <div class="btn-group" id="team-invite-btn" style="background-color: #f7f7f7;">
                         </div>
                     </div>
                 </div>
