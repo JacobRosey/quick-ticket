f957e8a896a7e65823388178f2aaba6242a1904b
JacobRosey <jrosey77@gmail.com>
2023-03-02 18:57:03 -0500

    css

--- public/index.js
+++ public/index.js
@@ -574,8 +574,8 @@ function useResponse(res) {
                     `
                     <div style="display: flex; flex-direction: row; justify-content: right;">
                         <span>`+res[i].team_name +`
-                        <a class="btn" onclick="teamInviteResponse(true, `+res[i]+`)" role="button" style="background-color: green; margin: .25em; margin-left: 1em; width: auto; line-height: 32.5px !important; padding: auto;"><i class="bi bi-check" style="color: white; font-weight: bolder; font-size: 1.5em;"></i></a>
-                        <a class="btn" onclick="teamInviteResponse(false, `+res[i]+`)" role="button" style="background-color: red; margin: .25em; width: auto; line-height: 32.5px !important; padding: auto;"><i class="bi bi-x" style="color: white; font-weight: bolder; font-size: 1.5em;"></i></a></span>
+                        <a class="btn" onclick="teamInviteResponse(true, `+res[i]+`)" role="button" style="background-color: green; margin: .25em; margin-left: 1em; width: auto; line-height: 32.5px !important;"><i class="bi bi-check" style="color: white; font-weight: bolder; font-size: 1.5em;"></i></a>
+                        <a class="btn" onclick="teamInviteResponse(false, `+res[i]+`)" role="button" style="background-color: red; margin: .25em; width: auto; line-height: 32.5px !important;"><i class="bi bi-x" style="color: white; font-weight: bolder; font-size: 1.5em;"></i></a></span>
                     </div>
                     `
             }
