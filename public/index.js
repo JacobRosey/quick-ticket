1f351f049347e126502d0095114607f29a659fa9
JacobRosey <jrosey77@gmail.com>
2023-03-02 18:38:43 -0500

    css

--- public/index.js
+++ public/index.js
@@ -572,10 +572,10 @@ function useResponse(res) {
             for(let i=0; i<res.length; i++){
                 inviteContainer.innerHTML +=
                     `
-                    <div style="display: flex; flex-direction: row; justify-content: center;">
+                    <div style="display: flex; flex-direction: row; justify-content: right;">
                         <span>`+res[i].team_name +`
-                        <a class="btn" onclick="teamInviteResponse(true, `+res[i]+`)" role="button" style="background-color: green; margin: .25em; font-weight: bolder; font-size: 1.5em; width: auto; line-height: 32.5px !important;"><i class="bi bi-check" style="color: white;"></i></a>
-                        <a class="btn" onclick="teamInviteResponse(false, `+res[i]+`)" role="button" style="background-color: red; margin: .25em; font-weight: bolder; font-size: 1.5em; width: auto; line-height: 32.5px !important;"><i class="bi bi-x" style="color: white;"></i></a></span>
+                        <a class="btn" onclick="teamInviteResponse(true, `+res[i]+`)" role="button" style="background-color: green; margin: .25em; width: auto; line-height: 32.5px !important;"><i class="bi bi-check" style="color: white; font-weight: bolder; font-size: 1.5em;"></i></a>
+                        <a class="btn" onclick="teamInviteResponse(false, `+res[i]+`)" role="button" style="background-color: red; margin: .25em; width: auto; line-height: 32.5px !important;"><i class="bi bi-x" style="color: white; font-weight: bolder; font-size: 1.5em;"></i></a></span>
                     </div>
                     `
             }
