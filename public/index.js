3a39769041824ea46d2a9b303ec6381a54fc3d5e
JacobRosey <jrosey77@gmail.com>
2023-03-03 14:05:59 -0500

    css

--- public/index.js
+++ public/index.js
@@ -558,7 +558,7 @@ function useResponse(res) {
             } else title = "You've received multiple team invitations!";
             container[0].insertAdjacentHTML('afterbegin', 
                 `
-            <div class="col-sm-auto">
+            <div class="col-sm-auto" style="height: auto;">
                 <div class="card text-center">
                     <div class="card-body">
                         <h5 class="card-title">`+ title + `</h5>
